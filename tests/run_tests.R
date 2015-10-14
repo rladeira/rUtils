cd20cc9a157dda74f28fd79cdce5ff40d413d321
rladeira <rwladeira@gmail.com>
2015-10-13 23:00:20 -0300

    refactor the unittests of the package

--- tests/run_tests.R
+++
@@ -1,8 +0,0 @@
-
-library(testthat)
-
-source(file.path("R", "sourceAllFiles.R"))
-
-sourceAllFiles(path = "R")
-
-test_dir("tests")
---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+
+library(testthat)
+library(rUtils)
+
+test_check("rUtils")
